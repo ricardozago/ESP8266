941287013de4c367ae7fedd56d474794d70eaeb6
Ricardo Zago <ricardozago91@gmail.com>
2017-10-18 13:20:45 -0200

    Add Webpage Arduino

---
+++ Arduino + ESP - página WEB/Ler_Portas/Ler_Portas.ino
@@ -0,0 +1,80 @@
+// https://www.filipeflop.com/blog/esp8266-arduino-tutorial/
+
+#include <SoftwareSerial.h>
+
+//RX pino 2, TX pino 3
+SoftwareSerial esp8266(2, 3);
+
+#define DEBUG true
+
+void setup()
+{
+  Serial.begin(19200);
+  esp8266.begin(19200);
+
+  sendData("AT+RST\r\n", 2000, DEBUG); // reset module
+  sendData("AT+CWMODE=2\r\n", 1000, DEBUG); // configure as access point
+  sendData("AT+CIFSR\r\n", 1000, DEBUG); // get ip address
+  sendData("AT+CIPMUX=1\r\n", 1000, DEBUG); // configure for multiple connections
+  sendData("AT+CIPSERVER=1,80\r\n", 1000, DEBUG); // turn on server on port 80
+}
+
+void loop()
+{
+  // Verifica se o ESP8266 esta enviando dados
+  if (esp8266.available())
+  {
+    if (esp8266.find("+IPD,"))
+    {
+      delay(300);
+      int connectionId = esp8266.read() - 48;
+
+      String webpage = "<head><meta http-equiv=""refresh"" content=""3"">";
+      webpage += "</head><h1><u>ESP8266 - Web Server</u></h1><h2>Porta";
+      webpage += "Digital 8: ";
+      int a = digitalRead(8);
+      webpage += a;
+      webpage += "<h2>Porta Digital 9: ";
+      int b = digitalRead(9);
+      webpage += b;
+      webpage += "</h2>";
+
+      String cipSend = "AT+CIPSEND=";
+      cipSend += connectionId;
+      cipSend += ",";
+      cipSend += webpage.length();
+      cipSend += "\r\n";
+
+      sendData(cipSend, 1000, DEBUG);
+      sendData(webpage, 1000, DEBUG);
+
+      String closeCommand = "AT+CIPCLOSE=";
+      closeCommand += connectionId; // append connection id
+      closeCommand += "\r\n";
+
+      sendData(closeCommand, 3000, DEBUG);
+    }
+  }
+}
+
+String sendData(String command, const int timeout, boolean debug)
+{
+  // Envio dos comandos AT para o modulo
+  String response = "";
+  esp8266.print(command);
+  long int time = millis();
+  while ( (time + timeout) > millis())
+  {
+    while (esp8266.available())
+    {
+      // The esp has data so display its output to the serial window
+      char c = esp8266.read(); // read the next character.
+      response += c;
+    }
+  }
+  if (debug)
+  {
+    Serial.print(response);
+  }
+  return response;
+}
